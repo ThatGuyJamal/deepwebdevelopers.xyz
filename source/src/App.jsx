192d4e62e39107b7014fea1d04ab5b68d2162844
Jamal L <jamallyons8@gmail.com>
2021-03-24 17:50:59 -0400

    Updating site

--- source/src/App.jsx
+++ source/src/App.jsx
@@ -1,14 +1,47 @@
-import React from "react";
-import LoadCustomRouter from './route/CustomRouter';
-import { HashRouter as Router } from "react-router-dom";
-function App() {
-	return (
-		<div>
-			<Router>
-				<LoadCustomRouter></LoadCustomRouter>
-			</Router>
-		</div>
-	);
+// import React from "react";
+// import LoadCustomRouter from './route/CustomRouter';
+// import { HashRouter as Router } from "react-router-dom";
+// function App() {
+// 	return (
+// 		<div>
+// 			<Router>
+// 				<LoadCustomRouter></LoadCustomRouter>
+// 			</Router>
+// 		</div>
+// 	);
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+// ! pages
+import LoadHomePage from "../components/home/HomePage";
+import LoadAboutPage from "../pages/views/about";
+import LoadProjectsPage from "../pages/views/projects";
+// ? mics
+import Footer from "../components/footer";
+import Navbar from "../components/navBar";
+import NF_404 from "../components/NF_404";
+
+export class App extends Component {
+	render() {
+		return (
+			<div>
+				<Router>
+					<Navbar />
+					<Switch>
+						<Route path="/" exact component={LoadHomePage} />
+						<Route path="/views/about" exact component={LoadAboutPage} />
+						<Route path="/views/projects" exact component={LoadProjectsPage} />
+						{/* <Route path="/" exact component={} /> */}
+						<Route path="*" component={NF_404} />
+					</Switch>
+					<Footer />
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
